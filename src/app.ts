5dd95f50a7aef534315bf6207dca26b923b1393f
Mounir Bennacer <benphaoris@gmail.com>
2024-10-06 14:15:14 +0100

    feat: adding pino logger, adding custom middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,22 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
 
+import notFound from "@/middlewares/not-found";
+import onError from "@/middlewares/on-error";
+import { pinoLogger } from "@/middlewares/pino-logger";
+
 const app = new OpenAPIHono();
+
+app.use(pinoLogger());
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
+
+app.get("/error", (c) => {
+  throw new Error("This is an error");
+});
+
+app.notFound(notFound);
+app.onError(onError);
 
 export default app;
